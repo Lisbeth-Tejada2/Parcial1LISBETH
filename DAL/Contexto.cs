482e22c85325a6b14de2cb97df77827094f1571a
Lisbeth Tejada <Latitude E7450@LISBETH>
2023-10-09 15:45:42 -0400

    Creando la migración

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,12 @@
-﻿namespace Parcial1LISBETH.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1LISBETH.Models;
+
+namespace Parcial1LISBETH.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Prestamo> prestamo { get; set; }
     }
 }
--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -1,6 +1,24 @@
-﻿namespace Parcial1LISBETH.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parcial1LISBETH.Models
 {
     public class Prestamo
     {
+        [Key]
+        public int PrestamoId { get; set; }
+        public DateTime Fecha { get; set; }
+
+        [Required(ErrorMessage = "Lo sentimos! El concepto es obligatorio")]
+        public string Concepto { get; set; }
+
+
+        [Required(ErrorMessage = "Lo sentimos! El monto es obligatorio")]
+        [Range(1, 1000000.00, ErrorMessage = "El monto debe ser entre {1} y {2}")]
+        public float Monto { get; set; }
+
+
+        [Required(ErrorMessage = "Lo sentimos!! El Balance es obligatorio")]
+        [Range(1, 2000000.00, ErrorMessage = "El Balance debe ser entre {1} y {2}")]
+        public float Balance { get; set; }
     }
 }
